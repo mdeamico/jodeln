58bce264203285d91ad3d8a946166b3dca33a7cb
mdeamico <33907265+mdeamico@users.noreply.github.com>
2021-07-24 17:16:42 -0500

    Remove nodes column that was for debugging

--- jodeln/gui/od_tablemodel.py
+++ jodeln/gui/od_tablemodel.py
@@ -20,14 +20,12 @@ class ODTableModel(QtCore.QAbstractTableModel):
                 return self._data[index.row()].d_name
             if index.column() == 2:
                 return self._data[index.row()].name
-            if index.column() == 3:
-                return str(self._data[index.row()].nodes)
 
     def rowCount(self, index):
         return len(self._data)
 
     def columnCount(self, index):
-        return 4
+        return 3
 
     def headerData(self, section: int, orientation: Qt.Orientation, role: int):
         """Get Table header names."""
@@ -38,8 +36,6 @@ class ODTableModel(QtCore.QAbstractTableModel):
                 return "Destination"
             if section == 2:
                 return "Route Name"
-            if section == 3:
-                return "Nodes"
 
     def get_route_at_index(self, index):
         """Return routes for the OD at the selected index."""
